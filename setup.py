535709ea87004b57e5368c7f7cf27d3086ac8e0d
Riswan Ahamed <riswanahamed38@gmail.com>
2024-08-03 17:49:02 +0530

    Setup initiated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "RisAhamed",
+    version ="0.0.0",
+    aurthor = "Riswan ahamed",
+    aurthor_email ="riswanahamed38@gmail.com",
+    packages=find_packages()
+)
